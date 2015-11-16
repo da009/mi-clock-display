25ca3941703a60c4b4c42515e738bcac479e9a25
dan009 <danielpraderasanjos@gmail.com>
2015-11-16 11:05:02 +0100

    Tener 2 constructores: uno sin parámetros, que fija la hora a 00:00 y otro con 2 parámetros de tipo int, que representan las horas y los minutos a los que fijar la hora actual

--- ClockDisplay.java
+++ ClockDisplay.java
@@ -1,13 +1,14 @@
 /**
  * Esta nueva clase debe:
- * Tener 2 atributos de tipo NumberDisplay, uno para las horas y otro para los minutos.
- * Tener un atributo de tipo String donde se almacena la hora actual del reloj con 5 caracteres.
- * Tener 2 constructores: uno sin parámetros, que fija la hora a "00:00" y otro con 2 parámetros de tipo int,
- * que representan las horas y los minutos a los que fijar la hora actual.
- * Tener un método setTime que acepte dos parámetros de tipo int, que representan horas y minutos,
+ * 1.- Tener 2 atributos de tipo NumberDisplay, uno para las horas y otro para los minutos.
+ * 2.- Tener un atributo de tipo String donde se almacena la hora actual del reloj con 5 caracteres.
+ * 3.- Tener 2 constructores: uno sin parámetros, que fija la hora a "00:00" y otro con 2 parámetros 
+ * de tipo int, que representan las horas y los minutos a los que fijar la hora actual.
+ * 4.- Tener un método setTime que acepte dos parámetros de tipo int, que representan horas y minutos,
  * y que fije dichos valores como el tiempo actual del reloj.
- * Tener un método getTime que devuelva la hora como String de 5 caracteres.
- * Tener un método timeTick que haga avanzar un minuto la hora actual (sea esta la que sea originalmente).
+ * 5.- Tener un método getTime que devuelva la hora como String de 5 caracteres.
+ * 6.- Tener un método timeTick que haga avanzar un minuto la hora actual (sea esta la que sea 
+ * originalmente).
  */
 public class ClockDisplay
 {
@@ -15,4 +16,26 @@ public class ClockDisplay
     private NumberDisplay horas;
     // Atributo minutos
     private NumberDisplay minutos;
+    // Atributo para la hora actual
+    private String horaac;
+    
+    /**
+     * Fija horas y minutos a 0
+     */
+    public ClockDisplay()
+    {
+        horas = new NumberDisplay(24);
+        minutos = new NumberDisplay(60);
+    }
+    
+    /**
+     * Horas y minutos a los que fijar la hora actual
+     */
+    public ClockDisplay (int horaac, int minutoac)
+    {
+        horas = new NumberDisplay(24);
+        minutos = new NumberDisplay(60);
+        horas.setValue(horaac);
+        minutos.setValue(minutoac);
+    }
 }
